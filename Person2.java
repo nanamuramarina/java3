a953e2442777343955395482c25ffca9ca05b541
nanamuramarina <nanamuramarina@gmail.com>
2023-12-10 13:45:15 +0000

    オーバーライド

---
+++ Person2.java
@@ -0,0 +1,16 @@
+public class Person2 {
+  private String name;
+
+  public void setName(String n) {
+    name = n;
+  }
+
+  public void display() {
+    System.out.println("名前：" + name);
+  }
+
+
+  public String getName() {
+    return name;
+  }
+}
